0816950821a232cdab6caf0d506f4a75f8fb0f81
magdalena krasicka <krasickamagdalena@gmail.com>
2023-08-25 11:00:21 +0100

    toggle cursor

--- main.js
+++ main.js
@@ -14,15 +14,22 @@
 // const indexTyping = setInterval(addLetter, time);
 
 const spnText = document.querySelector('.text');
-const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry.Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+const spnCursor = document.querySelector('.cursor')
+const text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
 
 let number = 0;
-const time = 70;
+const time = 40;
 
 function addLetter() {
+
   spnText.textContent += text[number];
   number++;
   if (number === text.length) clearInterval(index);
 }
 
+const cursorAnimation = () => {
+  spnCursor.classList.toggle('active');
+}
+
 const index = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
